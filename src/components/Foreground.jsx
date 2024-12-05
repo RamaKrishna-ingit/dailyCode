e1304edee1235622ec0b501f15376d21d409aa1f
thecodemarkog <thecodemarkog@gmail.com>
2024-12-05 23:33:35 +0530

    styling done

--- src/components/Foreground.jsx
+++ src/components/Foreground.jsx
@@ -1,14 +1,30 @@
-import React, { useState } from 'react'
+import React, { useRef} from 'react'
 import Card from './Card'
+
+
 function Foreground() {
+  const ref=useRef(null);
     const data =[
-        {desc:"hai gpr is the gtk of gkr opst ffflj clear?",filesize:".9mb", close:true, tag:{isOpen:true,tagTitle:"Download Now",tagColor:"green"}}
-    ]
-    useState()
+        {desc:"hai gpr is the gtk of gkr opst ffflj clear?",
+          filesize:".9mb", 
+          close:true, 
+          tag:{isOpen:true,tagTitle:"Download Now",tagColor:"blue"}
+        },
+        {desc:"hai gpr is the gtk of gkr opst ffflj clear?",
+          filesize:".9mb", 
+          close:true, 
+          tag:{isOpen:true,tagTitle:"Download Now",tagColor:"green"}
+        },
+        {desc:"hai gpr is the gtk of gkr opst ffflj clear?",
+          filesize:".9mb", 
+          close:true, 
+          tag:{isOpen:false,tagTitle:"Download Now",tagColor:"green"}
+        },
+    ];
   return (
-    <div className='fixed w-full h-full z-[3] top-0 left-0 '>
+    <div ref={ref} className='fixed w-full h-full z-[3] top-0 left-0 flex gap-10 flex-wrap p-5'>
         {data.map((item,index)=>(
-            <Card data={item}/>
+            <Card data={item} reference={ref}/>
         ))}
     </div>
 
